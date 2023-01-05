7f3b452e6cd0aa406a3beb378159e9b0d6d1a51b
leekyuyoung20221226 <122024737+leekyuyoung20221226@users.noreply.github.com>
2023-01-05 21:22:50 +0900

    Add files via upload

---
+++ my_calc.py
@@ -0,0 +1,12 @@
+def add(a,b):    
+    return a+b
+def minus(a,b):
+    return a-b
+def multi(a,b):
+    return a8b
+def divide(a,b):
+    return a/b
+
+# # 테스트용도로
+# if __name__. == '__main__':
+#     print(add(10,20))
